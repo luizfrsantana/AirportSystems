5f5f5c9fadb6b77dc78962a72480596842a132e5
luizfrsantana <luizfrsantana@gmail.com>
2024-03-23 21:52:10 +0000

    update global urls

--- AirportSystems/urls.py
+++ AirportSystems/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('FlightBookingApp.urls')),
     path('flightbooking/', include('FlightBookingApp.urls'))
 ]
